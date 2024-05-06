022b27f7b3693b735d8390d10829f1f04860cf56
Szabó Benedek <szabob@edu.bme.hu>
2024-05-06 23:11:15 +0200

    Another try

--- backend/config.js
+++ backend/config.js
@@ -1,12 +1,12 @@
 const config = {
+  DETECTION_HOST: process.env.DETECTION_HOST || "localhost",
+  DB_HOST: process.env.DB_HOST || "localhost",
+  DB_NAME: process.env.DB_NAME || "images",
+  DB_USER: process.env.DB_USER || "",
+  DB_PASSWORD: process.env.DB_PASSWORD || "",
   MQ_HOST: process.env.MQ_HOST || "localhost",
   MAIL_USER: process.env.MAIL_USER || "",
   MAIL_PASS: process.env.MAIL_PASS || "",
-  DETECTION_HOST: process.env.DETECTION_HOST || "localhost",
-  DB_HOST: process.env.DB_HOST || "localhost",
-  DB_USER: process.env.DB_USER || "",
-  DB_PASSWORD: process.env.DB_PASSWORD || "",
-  DB_NAME: process.env.DB_NAME || "images",
 };
 
 module.exports = config;
