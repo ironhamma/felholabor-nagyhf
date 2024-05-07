3ec0303d27e1f858cfbcf084c69a04d172d95541
Szabó Benedek <szabob@edu.bme.hu>
2024-05-07 22:19:59 +0200

    Change frontend env

--- backend/models/db.js
+++ backend/models/db.js
@@ -5,7 +5,7 @@ const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } = require("../config");
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
-  logging: false,
+  logging: true,
 });
 
 async function testConnection() {
