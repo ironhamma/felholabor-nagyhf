2cb464b2c09879f2b961ff7d812ca30637d8fd9c
Szabó Benedek <szabob@edu.bme.hu>
2024-05-08 20:47:27 +0200

    Lint issue

--- backend/source/getImages.js
+++ backend/source/getImages.js
@@ -11,7 +11,8 @@ const getImages = async (req, res) => {
       })),
     });
   } catch (error) {
-    
+    console.log(error);
+    res.status(500).json({ message: "Failed to get images" });
   }
 };
 
