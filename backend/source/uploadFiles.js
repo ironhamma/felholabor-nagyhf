0a3cba1dfa244e84643cbae36da2a20ea065b1d5
Szabó Benedek <szabob@edu.bme.hu>
2024-05-08 20:31:34 +0200

    Add try catch

--- backend/source/uploadFiles.js
+++ backend/source/uploadFiles.js
@@ -6,26 +6,31 @@ const { DETECTION_HOST } = require("../config");
 const { Image } = require("../models/db");
 
 async function uploadFiles(req, res) {
-  const formData = new FormData();
-  formData.append("image", fs.createReadStream(req.file.path));
-
-  const { data } = await axios.post(
-    `http://${DETECTION_HOST}/detect_cars`,
-    formData,
-    {
-      headers: { ...formData.getHeaders() },
-    },
-  );
-
-  const detection = data.cars;
-
-  const uploadedImages = await Image.create({
-    image: req.file.filename,
-    caption: req.body.caption,
-    detection,
-  });
-
-  res.json({ message: "Successfully uploaded files", uploadedImages });
+  try{
+    const formData = new FormData();
+    formData.append("image", fs.createReadStream(req.file.path));
+    
+    const { data } = await axios.post(
+      `http://${DETECTION_HOST}/detect_cars`,
+      formData,
+      {
+        headers: { ...formData.getHeaders() },
+      },
+    );
+    
+    const detection = data.cars;
+    
+    const uploadedImages = await Image.create({
+      image: req.file.filename,
+      caption: req.body.caption,
+      detection,
+    });
+    
+    res.json({ message: "Successfully uploaded files", uploadedImages });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Failed to upload files" });
+  }
 }
 
 exports.uploadFiles = uploadFiles;
