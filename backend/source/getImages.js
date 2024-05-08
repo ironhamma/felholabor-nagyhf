cc316be447a8653352f0ba85c701770f2d8882b4
Szabó Benedek <szabob@edu.bme.hu>
2024-05-08 20:43:45 +0200

    Add another try cath

--- backend/source/getImages.js
+++ backend/source/getImages.js
@@ -1,14 +1,18 @@
 const { Image } = require("../models/db");
 
 const getImages = async (req, res) => {
-  const images = await Image.findAll({ raw: true });
-  res.json({
-    images: images.map((image) => ({
-      image: image.image,
-      caption: image.caption,
-      detections: image.detection,
-    })),
-  });
+  try {
+    const images = await Image.findAll({ raw: true });
+    res.json({
+      images: images.map((image) => ({
+        image: image.image,
+        caption: image.caption,
+        detections: image.detection,
+      })),
+    });
+  } catch (error) {
+    
+  }
 };
 
 exports.getImages = getImages;
